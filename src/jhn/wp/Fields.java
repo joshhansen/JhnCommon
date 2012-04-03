e7113bba1879997d1090193e9317e9facaccb476
Josh Hansen <hansen.joshuaa@gmail.com>
2012-04-03 17:21:36 -0600

    Add an enum-ish class representing Lucene field names

---
+++ src/jhn/wp/Fields.java
@@ -0,0 +1,8 @@
+package jhn.wp;
+
+public final class Fields {
+	public static final String label = "label";
+	public static final String text = "text";
+	public static final String outboundLinks = "outbound_links";
+	public static final String containingCategories = "containing_categories";
+}
