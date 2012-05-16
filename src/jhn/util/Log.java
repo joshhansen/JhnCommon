326eebb6041b6dc87921f33d374561783f9e046b
Josh Hansen <hansen.joshuaa@gmail.com>
2012-05-16 14:04:53 -0600

    Bring jhn.util.Log in from EDA

---
+++ src/jhn/util/Log.java
@@ -0,0 +1,72 @@
+package jhn.util;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+
+public class Log {
+	
+	private PrintStream[] logs;
+	
+	public Log() {
+		this(System.out);
+	}
+	
+	public Log(String filename) throws FileNotFoundException {
+		this(System.out, filename);
+	}
+	
+	public Log(PrintStream logTo, String filename) throws FileNotFoundException {
+		this(logTo, new PrintStream(new FileOutputStream(filename)));
+	}
+	
+	public Log(PrintStream... logTo) {
+		this.logs = logTo;
+	}
+	
+	public void flush() {
+		for(PrintStream log : logs) {
+			log.flush();
+		}
+	}
+	
+	public void print(char c) {
+		for(PrintStream log : logs) {
+			log.print(c);
+		}
+		flush();
+	}
+	
+	public void print(Object o) {
+		for(PrintStream log : logs) {
+			log.print(o);
+		}
+		flush();
+	}
+	public void println(Object o) {
+		for(PrintStream log : logs) {
+			log.println(o);
+		}
+	}
+	public void println() {
+		for(PrintStream log : logs) {
+			log.println();
+		}
+	}
+	
+	public void println(int x) {
+		for(PrintStream log : logs) {
+			log.println(x);
+		}
+	}
+	
+	public void close() {
+		for(PrintStream log : logs) {
+			log.close();
+		}
+	}
+	
+	public PrintStream[] logs() {
+		return logs;
+	}
+}
