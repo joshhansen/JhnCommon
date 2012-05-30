d8f9a3c4255997b483e7b4b99171bb30fd88bc9c
Josh Hansen <hansen.joshuaa@gmail.com>
2012-05-30 17:57:54 -0600

    TopNTracker coming from EDA

---
+++ src/jhn/util/TopNTracker.java
@@ -0,0 +1,30 @@
+package jhn.util;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+
+public class TopNTracker {
+	private final Queue<Double> topNItems = new PriorityQueue<Double>();
+	private final int n;
+	public TopNTracker(final int n) {
+		this.n = n;
+	}
+	
+	public void add(double value) {
+		topNItems.add(value);
+		if(topNItems.size() > n) topNItems.remove();
+	}
+	
+	public List<Double> topN() {
+		List<Double> topN = new ArrayList<Double>(n);
+		while(!topNItems.isEmpty()) {
+			topN.add(topNItems.remove());
+		}
+		Collections.reverse(topN);
+		return topN;
+	}
+}
