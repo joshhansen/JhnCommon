d5ae6e245fafbef1ccd8f0a735b1dbc715a62deb
Josh Hansen <hansen.joshuaa@gmail.com>
2012-07-07 22:28:11 -0600

    Use the first N words as a "base" set. Further sets only include what is not in the base set. Presumably this will prevent the most frequent words from being duplicated across each set.

--- src/jhn/wp/visitors/ChunkedWordSetVisitor.java
+++ src/jhn/wp/visitors/ChunkedWordSetVisitor.java
@@ -12,13 +12,18 @@ import it.unimi.dsi.fastutil.objects.ObjectSet;
 
 public class ChunkedWordSetVisitor extends Visitor {
 	private static final String FILE_EXT = ".set";
+	
+	private ObjectSet<String> baseSet;
 	private ObjectSet<String> set;
+	private final int baseChunkSize;
 	private final int chunkSize;
 	private final File outputDir;
 	
-	public ChunkedWordSetVisitor(int chunkSize, String outputDir) {
+	public ChunkedWordSetVisitor(int baseChunkSize, int chunkSize, String outputDir) {
+		this.baseChunkSize = baseChunkSize;
 		this.chunkSize = chunkSize;
 		this.outputDir = new File(outputDir);
+		this.baseSet = newSet();
 		this.set = newSet();
 	}
 
@@ -28,18 +33,49 @@ public class ChunkedWordSetVisitor extends Visitor {
 	
 	@Override
 	public void visitWord(String word) {
-		set.add(word);
-		
-		if(set.size() >= chunkSize) {
-			writeSet();
-			set = newSet();
-			System.gc();
+		final int basePreSize = baseSet.size();
+		if(basePreSize < baseChunkSize) {
+			baseSet.add(word);
+			final int basePostSize = baseSet.size();
+			
+			if(basePostSize > basePreSize) {
+				if(basePostSize % 10000 == 0) {
+					System.out.print('b');
+					System.out.println(basePostSize);
+				}
+				
+				if(basePostSize >= baseChunkSize) {
+					writeSet(baseSet, nextFilename());
+				}
+			}
+		} else {
+			if(!baseSet.contains(word)) {
+				final int preSize = set.size();
+				set.add(word);
+				final int postSize = set.size();
+				
+				if(postSize > preSize) {
+					if(postSize % 10000 == 0) {
+						System.out.println(postSize);
+					}
+					
+					if(postSize >= chunkSize) {
+						writeSet();
+						set = newSet();
+						System.gc();
+					}
+				}
+			}
 		}
 	}
-
+	
 	private void writeSet() {
+		writeSet(set, nextFilename());
+	}
+	
+	private static void writeSet(ObjectSet<String> set, String filename) {
 		try {
-			BufferedWriter w = new BufferedWriter(new FileWriter(nextFilename()));
+			BufferedWriter w = new BufferedWriter(new FileWriter(filename));
 			String[] arr = set.toArray(new String[0]);
 			Arrays.sort(arr);
 			for(String s : arr) {
