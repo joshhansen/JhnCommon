98c296c928f0aef4e87be9f41fa78b243e045a32
Josh Hansen <hansen.joshuaa@gmail.com>
2012-07-09 23:43:39 -0600

    Index that loads a bunch of strings from disk and uses binary search to figure out the position of the string in question

---
+++ src/jhn/idx/DiskStringIndex.java
@@ -0,0 +1,79 @@
+package jhn.idx;
+
+import it.unimi.dsi.fastutil.objects.ObjectArrayList;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+import jhn.Paths;
+
+/** The backing file must be sorted (ascending) */
+public class DiskStringIndex implements ReverseIndex<String> {
+	private String[] arr;
+	public DiskStringIndex(String filename) throws IOException {
+//		arr = new String[]{"cat", "dog", "zebra"};
+		BufferedReader r = new BufferedReader(new FileReader(filename));
+		
+		ObjectArrayList<String> list = new ObjectArrayList<String>();
+		String line = null;
+		while( (line=r.readLine()) != null) {
+			list.add(line);
+		}
+		
+		r.close();
+		
+		System.out.println("Loaded");
+		arr = list.toArray(new String[0]);
+		System.out.println("Array'd");
+	}
+
+	@Override
+	public int indexOf(String value) {
+		int left = 0;
+		int right = arr.length;
+		int mid;
+		int cmp;
+		while(left <= right) {
+			mid = (left + right) / 2;
+			
+			cmp = arr[mid].compareTo(value);
+			if(cmp < 0) {
+				left = mid + 1;
+			} else if(cmp > 0) {
+				right = mid - 1;
+			} else {
+				return mid;
+			}
+		}
+		return KEY_NOT_FOUND;
+	}
+
+
+	@Override
+	public int indexOf(String value, boolean addIfNotPresent) {
+		if(addIfNotPresent) {
+			throw new UnsupportedOperationException("Can't add to this index");
+		}
+		return indexOf(value);
+	}
+
+
+	@Override
+	public boolean contains(String value) {
+		return indexOf(value) != KEY_NOT_FOUND;
+	}
+
+	public static void main(String[] args) throws IOException {
+		String filename = Paths.outputDir("JhnCommon")+"/word_sets/chunks/19.set";
+		DiskStringIndex idx = new DiskStringIndex(filename);
+		System.out.print("dog: ");
+		System.out.println(idx.indexOf("dog"));
+		System.out.print("music: ");
+		System.out.println(idx.indexOf("music"));
+		System.out.print("zebra: ");
+		System.out.println(idx.indexOf("zebra"));
+		System.out.print("^^^^^^: ");
+		System.out.println(idx.indexOf("^^^^^^"));
+	}
+}
