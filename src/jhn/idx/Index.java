c2a19654cf4ace3a33baa518b367fd56d255b9c1
Josh Hansen <hansen.joshuaa@gmail.com>
2012-08-02 14:26:47 -0600

    Comments in empty blocks

--- src/jhn/idx/Index.java
+++ src/jhn/idx/Index.java
@@ -2,5 +2,5 @@ package jhn.idx;
 
 
 public interface Index<T> extends ForwardIndex<T>, ReverseIndex<T> {
-	
+	// This interface's methods are the union of the respective methods of ForwardIndex and ReverseIndex.
 }
--- src/jhn/idx/IntIndex.java
+++ src/jhn/idx/IntIndex.java
@@ -2,5 +2,5 @@ package jhn.idx;
 
 
 public interface IntIndex extends Index<Integer>, ForwardIntIndex, ReverseIntIndex {
-
+	// This interface is the int-specific combination of forward and reverse indexing
 }
